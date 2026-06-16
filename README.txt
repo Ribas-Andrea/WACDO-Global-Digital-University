976950561aaed5601c09e7e2b0edd47ef4edb9e5
Ribas Andréa <ribas.andrea17800@gmail.com>
2026-06-16 15:56:20 +0200

    Gestion des quantité du panier et ajout de l'évènement au clic pour la suppression

--- README.txt
+++ README.txt
@@ -42,3 +42,4 @@ Etapes avancement github :
 29/ Isolement des fonction du panier dans le fichier javascript dédié
 30/ Renommage fonction defilerPopupMenus + modification emplacement setItem pour le type de menu et l'accompagnement du local storage
 30/ Gérer l'affichage du menu dans le panier correctement
+31/ Gestion des quantité du panier et ajout de l'évènement au clic pour la suppression
--- js/panier.js
+++ js/panier.js
@@ -21,10 +21,6 @@ if (mode === "emporter") {
 afficherNumeroCommande()
 
 
-
-
-
-
 function ajouterAuPanier() {
   const menu = localStorage.getItem("memoireMenu");
   const burger = localStorage.getItem("memoireBurger");
@@ -36,13 +32,27 @@ function ajouterAuPanier() {
     return;
   }
 
-  panier.push({
-    quantite: 1,
-    type: { menu, burger },
-    options: { accompagnement, boisson }
-  });
+    // On recherche s'il y a un produit identique : (on met p pour le nom d'une variable)
+  const produitExistant = panier.find(p =>
+    p.type.menu === menu &&
+    p.type.burger === burger &&
+    p.options.boisson === boisson &&
+    p.options.accompagnement === accompagnement
+  );
 
+  if (produitExistant) {
+    // Si le produit existe déjà alors on augmente la quantité : 
+    produitExistant.quantite += 1;
+  } else {
+    // sinon on créer le nouveau produit : 
+    panier.push({
+      quantite: 1,
+      type: { menu, burger },
+      options: { accompagnement, boisson }
+    });
+  }
   console.log("PANIER AJOUTÉ :", panier);
+  afficherPanier();
 }
 
 function afficherPanier() {
@@ -60,7 +70,7 @@ function afficherPanier() {
     return;
   }
 
-  panier.forEach((article) => {
+  panier.forEach((article, index) => {
     const prodPanier = document.createElement('article');
     prodPanier.classList.add('produits-panier');
 
@@ -76,6 +86,9 @@ function afficherPanier() {
     logoSupp.classList.add('logo-trash');
     logoSupp.src = './assets/trash.png';
     logoSupp.alt = 'Supprimer';
+    logoSupp.addEventListener('click', () =>{
+      supprimerPanier(index);
+    })
 
     const listPanier = document.createElement('ul');
     listPanier.classList.add('liste-detail-produits');
@@ -97,3 +110,29 @@ function afficherPanier() {
 }
 
 afficherPanier();
+
+
+
+function changerQuantite(index, quantity){
+  // trouver le produit dans le panier : 
+  const produit = panier[index];
+  if (!produit) return; // si le produit n'existe pas on arrête la fonction
+ const nouvelleQuantite =  produit.quantite + quantity;
+   
+  if (nouvelleQuantite <= 0) {
+    panier.splice(index, 1);
+  } else {
+    produit.quantite = nouvelleQuantite;
+  }
+
+  afficherPanier();
+}
+
+
+function supprimerPanier(index) {
+  if (index < 0 || index >= panier.length) return;
+
+  panier.splice(index, 1);
+
+  afficherPanier();
+}
